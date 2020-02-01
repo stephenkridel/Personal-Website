f0e3b09c1eabd20315e935ee2f072aebbc8ba9f5
Stephen <stephen.kridel@gmail.com>
2020-02-01 11:09:00 -0500

    add 2 event listeners to make the three-bar icon change color when hovered over

--- sidebar.js
+++ sidebar.js
@@ -1,8 +1,16 @@
+// declaring the three bar icon as a variable
 var sidebarIcon = document.getElementById("sidebarIcon");
 
+// calling a Semantic Ui function to open the menu/navbar when sidebarIcon is clicked
 sidebarIcon.addEventListener('click', function (){
-    $('.ui.left.vertical.menu.sidebar')
+    $('.ui.top.vertical.menu.sidebar')
     .sidebar('toggle');
 });
 
-alert("working")
+// changing the three bar icon's color when the mouse is hovered over it
+sidebarIcon.addEventListener('mouseenter', function(){
+    sidebarIcon.style.color = "grey";
+});
+sidebarIcon.addEventListener('mouseleave', function(){
+    sidebarIcon.style.color = "black";
+});
