7cc929832c07ed1e0ba70aaf07832464ff1634b2
Stephen <stephen.kridel@gmail.com>
2020-03-01 10:59:04 -0500

    Refactored dimmer.js to remove Semantic Ui dimmer and Jquery and rewrote it in vanilla js. Utilized a close button to more reliably managed states. Added all of the html and css for it to look nice and be optimized for mobile. CSS could use more semantic naming or commenting.

--- dimmer.js
+++ dimmer.js
@@ -1,17 +1,17 @@
 // creating an object that associates a card and its corresponding dimmer
-var Fall2017Obj     = {container: document.getElementById("containerFall2017"), card: document.getElementById("cardFall2017"), dimmer: $('.Fall2017')},
-    Sum2018Obj      = {container: document.getElementById("containerSum2018"), card: document.getElementById("cardSum2018"), dimmer: $('.Sum2018')},
-    Spr2019Obj      = {container: document.getElementById("containerSpr2019"), card: document.getElementById("cardSpr2019"), dimmer: $('.Spr2019')},
-    Fall2019Obj     = {container: document.getElementById("containerFall2019"), card: document.getElementById("cardFall2019"), dimmer: $('.Fall2019')},
-    // yeah I know "why did you use jquery AND vanilla js!?"" Well I couldn't get it to work with using querySelector to capture the dimmer element and then use the semantic ui method 'dimmer' on it. Oh well.
+var Fall2017Obj     = {container: document.getElementById("containerFall2017"), card: document.getElementById("cardFall2017")},
+    Sum2018Obj      = {container: document.getElementById("containerSum2018"), card: document.getElementById("cardSum2018")},
+    Spr2019Obj      = {container: document.getElementById("containerSpr2019"), card: document.getElementById("cardSpr2019")},
+    Fall2019Obj     = {container: document.getElementById("containerFall2019"), card: document.getElementById("cardFall2019")},
     
     // an array of each object to allow looping through
     objArray        = [Fall2017Obj, Sum2018Obj, Spr2019Obj, Fall2019Obj],
 
-    //  the main content window
-    coopContent     = document.getElementById('coopContent'),
     // the class each dimmer has. Starts out with Display: none because it overlays the rest of the content.
-    container       = document.getElementsByClassName('dimmer container')
+    container       = document.getElementsByClassName('dimmer container'),
+
+    // the button element that appears when a dimmer is activated and allows you to run hideDimmer()
+    closeButton     = document.getElementsByClassName('big close icon')
 ;
 
 // function that allows you to easily find a card's corresponding dimmer by inputting an array and the dimmer's card
@@ -24,37 +24,38 @@ function getDimmerByCard(array, card){
     return neededDimmer;
 };
 
-// main loop that adds the clicker event to each card
-for(i = 0; i < objArray.length; i++){
-    // adding an event listener to every card
-    objArray[i].card.addEventListener("click", function(){
-
-        // hiding every card's dimmer
-        for(i = 0; i < objArray.length; i++){
-            objArray[i].dimmer.dimmer('hide'); // the first dimmer is the dimmer defined in objArray, the second dimmer is a Semantic Ui method
-        };
-        // dimming the card that is clicked
-        clickedDimmer = getDimmerByCard(objArray, this);
-        clickedDimmer.dimmer.dimmer('show'); // the first dimmer is the dimmer defined in objArray, the second dimmer is a Semantic Ui method
-    });
+// function that allows closure of dimmer
+function hideDimmer(){
+    for(i = 0; i < objArray.length; i++){
+        closeButton[i].addEventListener('click', function(){
+            clickedDimmer.container.style.display = 'none';
+        });
+    };
 };
 
-function unDim (){
-    setTimeout(function(){
-        for(i = 0; i < objArray.length; i++){
-            if(objArray[i].container.classList.contains('dimmed')){
-                objArray[i].container.style.display = "inherit";
-            } else {
-                objArray[i].container.style.display = "none";
-            };
-        };
-        console.log('check')
-    }, 100)
+// function that fades in dimmer
+function showDimmer(){
+    for(i = 0; i < objArray.length; i++){
+        objArray[i].card.addEventListener("click", function(){
+            // dimming the card that is clicked
+
+            clickedDimmer = getDimmerByCard(objArray, this);
+
+            // this block increases the opacity of the container by 0.025 each step until its 1. (Background-color is rgba(0,0,0,0.9))
+            var op = 0.1;  // initial opacity
+            var timer = setInterval(function () {
+                if (op >= 1){
+                    clearInterval(timer);
+                }
+                clickedDimmer.container.style.display = 'flex';
+                clickedDimmer.container.style.opacity = op
+                op += 0.025;
+            }, 2.5);
+
+        });
+    };
 };
 
-coopContent.addEventListener('click', unDim);
-coopContent.addEventListener('touchstart', unDim);
+showDimmer();
 
-for(i = 0; i < objArray.length; i++){
-    objArray[i].container.addEventListener('mouseover', unDim);
-};
+hideDimmer();
