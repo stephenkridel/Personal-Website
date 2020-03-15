3de1742c07839d5ea8756ebdd31ba00668309544
Stephen <stephen.kridel@gmail.com>
2020-03-14 22:46:53 -0400

    Moved the dark theme toggler to the side bar. Changed sizes of cards in coopContent and other minor changes

--- scripts/DarkTheme.js
+++ scripts/DarkTheme.js
@@ -7,7 +7,8 @@ var toggleSlider        = document.getElementById("darkThemeToggler"),
     subHeader4          = document.getElementById("sub_header_4"),
     body                = document.querySelector("body"),
     aboutMeHeader       = document.getElementById("aboutMeHeader"),
-    bottomBar           = document.getElementById("bottomBar")
+    bottomBar           = document.getElementById("bottomBar"),
+    darkLabel           = document.getElementById("darkLabel")
 ;  
 
 // checks if the toggler is checked when the DOM loads and toggles dark theme if it is
@@ -27,12 +28,21 @@ toggleSlider.addEventListener("click", toggleDarkThemes);
 // function that assigns appropriate classes
 function toggleDarkThemes(){
     sidebar.classList.toggle("inverted");
-    menu.classList.toggle("inverted");    
+    menu.classList.toggle("inverted");   
     pusher.classList.toggle("darkifyPusher");
     nameHeader.classList.toggle("darkifyHeader");
     subHeader2.classList.toggle("darkifyHeader");
     subHeader4.classList.toggle("darkifyHeader");
     body.classList.toggle("darkifyPusher");
     aboutMeHeader.classList.toggle("darkifyPortraitHeader");
-    bottomBar.classList.toggle("blackBar");
+    darkLabel.classList.toggle("darkText");
+
+    checked = $('.ui.checkbox.toggle input').is(':checked');
+    // console.log(checked);
+    if (checked) {
+        darkLabel.textContent = "";
+    }
+    else {
+        darkLabel.textContent = "Dark Theme";
+    }
 };
